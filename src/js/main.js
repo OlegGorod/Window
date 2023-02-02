0bb917fe0bcafecf1d2e1b39fdc99463a501f8c4
OlegGorodenskyi <olgo@owort.com>
2023-02-02 17:22:52 +0200

    made timer

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,13 @@ import './slider';
 import modal from './modules/modal.js';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', () => {
     modal();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     forms();
+    timer();
 });
 
